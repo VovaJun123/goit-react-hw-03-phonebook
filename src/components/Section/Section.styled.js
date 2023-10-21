0986fc1cfe76e7fa6a7d21a325810c1b6c3210db
VovaJun123 <121401616+VovaJun123@users.noreply.github.com>
2023-10-21 16:10:40 +0100

    add

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  width: 520px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 12px;
+  margin: 20px auto 0 auto;
+  border: 2px solid #0e0d0d;
+  border-radius: 6px;
+  background-color: #426842;
+`;
